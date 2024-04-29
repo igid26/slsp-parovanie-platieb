2f262d4c95254fa09347bba97fc2625342158db5
igid26 <majan261166@gmail.com>
2024-04-29 11:46:43 +0200

    úprava porovnávania

--- index.php
+++ index.php
@@ -25,21 +25,17 @@ $password = '.....'; //heslo k emailu kam chodia výpisy
 $searchArray = array('SUBJECT'=>'Výpis ..... - ', 'SINCE'=>date('j F Y',strtotime('4 month ago')));  //Sem zadajte text predmetu emailu, respektíve výpisu. Väčšinou je to v tvare Výpis ..... Pomocou tohto identifikujete emaily, ktoré má spracúvať
 
 //Save attachement file to 
-$saveToPath = plugin_dir_path( __FILE__ ) . '/dump/'; //change this
+$saveToPath = plugin_dir_path( __FILE__ ) . '/dump/'; //Sem bude ukladať zip
 //Extract zip files to
-$unzipDest = plugin_dir_path( __FILE__ ) .'/files/'; //change this
+$unzipDest = plugin_dir_path( __FILE__ ) .'/files/'; //sem bude ukladať rozbalené súbory
 
 
-require "funkcia-emailu.php";
+require "funkcia-emailu.php"; //načítanie funkcie na spracovanie emailov
 
 //Create an object
 $xa = new exAttach($hostname,$username,$password);
 $xa->get_files($searchArray, $saveToPath);
 $xa->extract_zip_to($unzipDest);
-
-
-
-
 
 
 $dir = plugin_dir_path( __FILE__ ) .'/files/';
@@ -54,15 +50,12 @@ if (is_dir($dir)) {
   }
 }
 //print_r($fille_array);
-$nazov_suboru = max($fille_array);
+$nazov_suboru = max($fille_array); //spracuje rozbalený a sprovaný súbor s najväčším číslom v názve
 
 $xml = simplexml_load_file($unzipDest . $nazov_suboru);
 
 
 
-//echo (string)$xml->GrpHdr->MsgRcpt->PstlAdr['PstCd'] . '<br />';
-
-
 
 $zaklad = $xml->BkToCstmrStmt->Stmt->Ntry;
 
@@ -70,12 +63,9 @@ $xml = simplexml_load_file($unzipDest . $nazov_suboru);
 
 
 
-//echo (string)$xml->GrpHdr->MsgRcpt->PstlAdr['PstCd'] . '<br />';
-
 $zaklad = $xml->BkToCstmrStmt->Stmt->Ntry;
 
 
-$zaklad = $xml->BkToCstmrStmt->Stmt->Ntry;
 
 
 $rozdelenie = '';
@@ -106,17 +96,17 @@ echo $num . '<br>';
 $vyriabilne_simboly[] = $num;
 }
 
-//print_r($vyriabilne_simboly);
+//print_r($vyriabilne_simboly); //vytlaci všetky variabilné symboly
 
 array_push($vyriabilne_simboly,"88853061", "88853058"); 
-//array_push($vyriabilne_simboly_nove,"77753078");
 
 
+//Teraz potrebujeme vypísať posledných 1000 objednávok a následne ich budeme porovnávať podľa ID objednávky. 
 $vsetky_objednavky = array();
 $loop = new WP_Query( array(
     'post_type'         => 'shop_order',
     'post_status'       =>  'wc-on-hold',
-    'posts_per_page'    => 10000,
+    'posts_per_page'    => 1000,
     'orderby' => 'date', 
     'order' => 'DESC'
 
@@ -170,24 +160,13 @@ endif; ?>
 
 
 
-print_r($vsetky_objednavky);
-
-print_r($result);
-
 foreach($result as $results) {
 echo $results . '<br><br>';  
 $nove_idcko = substr($results, 3);
 $order = new WC_Order($nove_idcko);
-$order->update_status('completed', 'order_note');
+$order->update_status('completed', 'order_note');  //aktualizovanie statusu objednávky na vybavené
 }
-           
-
-
-
-
-
-
-           
+                  
 
 } 
 
@@ -199,53 +178,14 @@ include ('manualne2.php');
 
 
 
-function filter_woocommerce_order_number( $order_number, $order ) {
-    // Loop through order items
-    foreach ( $order->get_items() as $item ) {
-        // Product ID
-        $product_id = $item->get_variation_id() > 0 ? $item->get_variation_id() : $item->get_product_id();
-
-        // Has term (product category)
-        if ( has_term( array( 'omsove-milodary', 'pod-kategoria' ), 'product_cat', $product_id ) ) {
-            return '777' . $order_number;
-        } elseif ( has_term( array( 'misijne-predmety' ), 'product_cat', $product_id ) ) {
-            return '888' . $order_number;
-        } elseif ( has_term( array( 'sladka-pomoc-2022' ), 'product_cat', $product_id ) ) {
-            return '444' . $order_number;
-        } elseif ( has_term( array( 'omsa-ako-dar-2022', 'podkategoria-pohladnice-2022' ), 'product_cat', $product_id ) ) {
-            return '500' . $order_number;
-        } elseif ( has_term( array( 'omsa-ako-dar-2023', 'podkategoria-pohladnice-2023' ), 'product_cat', $product_id ) ) {
-            return '500' . $order_number;
-        } else  {
-            return $order_number;
-        }
-    }
-    
-    return $order_number;
-}
-add_filter( 'woocommerce_order_number', 'filter_woocommerce_order_number', 10, 2 );
 
 
-
-
-
+//vytvorenie cronjobu - raz za deň o pol 1 ráno, kedy nie je zaťažený server.
 
 if( !wp_next_scheduled('ovrerenie_platby_slsp' ) ) 
-   wp_schedule_event( strtotime('00:10:00'), 'daily', 'ovrerenie_platby_slsp' );
+   wp_schedule_event( strtotime('00:30:00'), 'daily', 'ovrerenie_platby_slsp' );
  
 add_action( 'ovrerenie_platby_slsp', 'napojenie_na_slsp', 10, 3 );
-
-
-if( !wp_next_scheduled('ovrerenie_platby_slsp2' ) ) 
-   wp_schedule_event( strtotime('00:30:00'), 'daily', 'ovrerenie_platby_slsp2' );
- 
-add_action( 'ovrerenie_platby_slsp2', 'napojenie_na_slsp', 10, 3 );
-
-
-if( !wp_next_scheduled('ovrerenie_platby_slsp3' ) ) 
-   wp_schedule_event( strtotime('00:50:00'), 'daily', 'ovrerenie_platby_slsp3' );
- 
-add_action( 'ovrerenie_platby_slsp3', 'napojenie_na_slsp', 10, 3 );
 
 
 
